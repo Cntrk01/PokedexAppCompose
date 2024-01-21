02195062f0686ee5bf12edc0180512aede5e81c6
Mustafa Cantürk Karabulut <karabulut_3828@hotmail.com>
2024-01-21 20:04:33 +0300

    ad
    asd

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -89,6 +89,7 @@ dependencies {
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
 
+    
     // Coroutines
     implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
     implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
